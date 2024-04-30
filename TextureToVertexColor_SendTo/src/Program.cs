7503264006ead3623f0b80433b3149477417ac73
Charlie Rix <charlie.rix@gmail.com>
2024-04-30 16:59:48 -0500

    created sendto project

---
+++ TextureToVertexColor_SendTo/src/Program.cs
@@ -0,0 +1,14 @@
+﻿if (args == null || args.Length == 0)
+    Console.WriteLine("No arguments passed in");
+else
+    foreach (string arg in args)
+        Console.WriteLine(arg);
+
+
+string link_name = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.SendTo), "TextureToVertexColor.lnk");
+string exe_name = System.IO.Path.Combine(Environment.CurrentDirectory, "TextureToVertexColor_SendTo.exe");
+
+Console.WriteLine($"link_name: {link_name}");
+Console.WriteLine($"exe_name: {exe_name}");
+
+Console.ReadKey();
