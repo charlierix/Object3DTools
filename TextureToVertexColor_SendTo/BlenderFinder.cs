b718488f8415d1d9968c615953f0d10f6142bcef
Charlie Rix <charlie.rix@gmail.com>
2024-05-01 21:41:38 -0500

    removed unused usings

--- TextureToVertexColor_SendTo/BlenderFinder.cs
+++ TextureToVertexColor_SendTo/BlenderFinder.cs
@@ -1,11 +1,4 @@
-﻿using Microsoft.Extensions.FileSystemGlobbing;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using static System.Runtime.InteropServices.JavaScript.JSType;
+﻿using System.Text.RegularExpressions;
 
 namespace TextureToVertexColor_SendTo
 {
